f5c7ed98f16a8f7d0d2f6d6f98bbab9e3b3027b3
turtton <top.gear7509@turtton.net>
2023-07-10 10:13:38 +0900

    :construction: Create value objects

---
+++ kernel/src/entities/account/id.rs
@@ -0,0 +1,22 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq, Serialize, Deserialize)]
+struct AccountId(i64);
+
+impl AccountId {
+    pub fn new(id: impl Into<i64>) -> Self {
+        Self(id.into())
+    }
+}
+
+impl From<AccountId> for i64 {
+    fn from(value: AccountId) -> Self {
+        value.0
+    }
+}
+
+impl AsRef<i64> for AccountId {
+    fn as_ref(&self) -> &i64 {
+        &self.0
+    }
+}
