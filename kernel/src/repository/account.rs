844ef639622d864669f57490467e3f61d918e10a
turtton <top.gear7509@turtton.net>
2023-10-04 18:32:15 +0900

    :hammer: Add update method
    :art: Format codes

--- kernel/src/repository/account.rs
+++ kernel/src/repository/account.rs
@@ -1,14 +1,15 @@
-use crate::{entity::{Account, Id, AccountName}, error::KernelError};
+use crate::{
+    entity::{Account, AccountName, Id},
+    error::KernelError,
+};
 
 pub trait AccountRepository: 'static + Sync + Send {
     fn find_by_id(&self, id: &Id<Account>) -> Result<Option<Account>, KernelError>;
-    fn find_by_stellar_id(
-        &self,
-        stellar_id: &Id<Account>,
-    ) -> Result<Option<Account>, KernelError>;
+    fn find_by_stellar_id(&self, stellar_id: &Id<Account>) -> Result<Option<Account>, KernelError>;
     fn find_by_name(&self, name: &AccountName) -> Result<Option<Account>, KernelError>;
     fn save(&self, account: &Account) -> Result<(), KernelError>;
-    fn delete(&self, account: &Account) -> Result<(), KernelError>;
+    fn update(&self, account: &Account) -> Result<(), KernelError>;
+    fn delete(&self, id: &Id<Account>) -> Result<(), KernelError>;
 }
 
 pub trait DependOnAccountRepository: 'static + Sync + Send {
