75846eefdb456fe91754700407ef623a5f4fb347
turtton <top.gear7509@turtton.net>
2024-07-04 22:40:27 +0900

    :hammer: Update error enum

--- kernel/src/error.rs
+++ kernel/src/error.rs
@@ -1,19 +1,21 @@
-#[derive(Debug, thiserror::Error)]
+use error_stack::Context;
+use std::fmt::{Display, Formatter};
+
+#[derive(Debug)]
 pub enum KernelError {
-    #[error("cannot find `{id}:{entity}` in the following {method}.")]
-    NotFound {
-        method: &'static str,
-        entity: &'static str,
-        id: String,
-    },
-    #[error("invalid value `{value}` in the following {method}.")]
-    InvalidValue { method: &'static str, value: String },
-    #[error(transparent)]
-    Serde(anyhow::Error),
-    #[error(transparent)]
-    Parse(anyhow::Error),
-    #[error(transparent)]
-    Driver(anyhow::Error),
-    #[error(transparent)]
-    External(anyhow::Error),
+    Concurrency,
+    Timeout,
+    Internal,
 }
+
+impl Display for KernelError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            KernelError::Concurrency => write!(f, "Concurrency error"),
+            KernelError::Timeout => write!(f, "Process Timed out"),
+            KernelError::Internal => write!(f, "Internal kernel error"),
+        }
+    }
+}
+
+impl Context for KernelError {}
