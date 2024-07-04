83f3724af3b9dfdde29f8b1661defce448e74618
turtton <top.gear7509@turtton.net>
2024-07-04 22:44:20 +0900

    :hammer: Update Driver error

--- driver/src/error.rs
+++ driver/src/error.rs
@@ -1,18 +1,6 @@
 use kernel::KernelError;
 
-#[derive(Debug, thiserror::Error)]
-pub enum DriverError {
-    #[error(transparent)]
-    SqlX(#[from] sqlx::Error),
-    #[error(transparent)]
-    Kernel(#[from] KernelError),
-}
-
-impl From<DriverError> for KernelError {
-    fn from(value: DriverError) -> Self {
-        match value {
-            DriverError::SqlX(e) => KernelError::Driver(anyhow::Error::new(e)),
-            DriverError::Kernel(e) => e,
-        }
-    }
+pub trait ConvertError {
+    type Ok;
+    fn convert_error(self) -> error_stack::Result<Self::Ok, KernelError>;
 }
