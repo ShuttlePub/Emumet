93e24156b8fd02816580db3da5a6339360417e40
turtton <top.gear7509@turtton.net>
2024-07-04 22:41:42 +0900

    :hammer: Add DeletedAt

---
+++ kernel/src/entity/common/deleted_at.rs
@@ -0,0 +1,37 @@
+use serde::{Deserialize, Deserializer, Serialize};
+use std::marker::PhantomData;
+use time::OffsetDateTime;
+use vodca::{AsRefln, Fromln};
+
+#[derive(Debug, Clone, Hash, Fromln, AsRefln)]
+pub struct DeletedAt<T>(OffsetDateTime, PhantomData<T>);
+
+impl<T> DeletedAt<T> {
+    pub fn new(time: impl Into<OffsetDateTime>) -> Self {
+        Self(time.into(), PhantomData)
+    }
+}
+
+impl<T> Default for DeletedAt<T> {
+    fn default() -> Self {
+        Self(OffsetDateTime::now_utc(), PhantomData)
+    }
+}
+
+impl<T> Serialize for DeletedAt<T> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        self.0.serialize(serializer)
+    }
+}
+
+impl<'de, T> Deserialize<'de> for DeletedAt<T> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        <OffsetDateTime>::deserialize(deserializer).map(|time| Self::new(time))
+    }
+}
