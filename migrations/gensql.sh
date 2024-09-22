88e9b8081ab275a63ec81075294361977eba6209
turtton <top.gear7509@turtton.net>
2024-09-22 23:29:23 +0900

    :card_file_box: Update database

--- migrations/gensql.sh
+++ migrations/gensql.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-curl -o 20230707210300_init.dbml https://raw.githubusercontent.com/ShuttlePub/document/b05e336ad10a94a429e7afa13f5a6ac8f4dc0871/packages/document/dbml/emumet.dbml
+curl -o 20230707210300_init.dbml https://raw.githubusercontent.com/ShuttlePub/document/47156f657afa75d459f9f557a09253a2a93e6b71/packages/document/dbml/emumet.dbml
 pnpm --package=@dbml/cli dlx dbml2sql 20230707210300_init.dbml -o 20230707210300_init.sql
