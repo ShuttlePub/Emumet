8e2e0f5a493b6e71792de34366b80933751342a0
turtton <top.gear7509@turtton.net>
2025-02-13 22:26:44 +0900

    :wrench: Add server as package

--- flake.nix
+++ flake.nix
@@ -11,9 +11,19 @@
       pkgs = import nixpkgs {
         inherit system;
       };
+      emumet = pkgs.rustPlatform.buildRustPackage {
+        pname = "server";
+        name = "emumet";
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+        nativeBuildInputs = [ pkgs.pkg-config ];
+        buildInputs = [ pkgs.openssl ];
+        PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+      };
     in
     with pkgs; rec {
       formatter = nixpkgs-fmt;
+      packages.default = emumet;
       devShells.default = mkShell {
         nativeBuildInputs = [ pkg-config ];
         buildInputs = [ openssl ];
