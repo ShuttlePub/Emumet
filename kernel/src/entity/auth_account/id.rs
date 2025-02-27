2bf4ac871f4f79722038e9e982d89b776e28b5b0
turtton <top.gear7509@turtton.net>
2025-02-28 00:45:14 +0900

    :hammer: Add defaults

--- kernel/src/entity/auth_account/id.rs
+++ kernel/src/entity/auth_account/id.rs
@@ -6,6 +6,12 @@ use vodca::{AsRefln, Fromln, Newln};
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Fromln, AsRefln, Newln, Serialize, Deserialize)]
 pub struct AuthAccountId(Uuid);
 
+impl Default for AuthAccountId {
+    fn default() -> Self {
+        AuthAccountId(Uuid::now_v7())
+    }
+}
+
 impl From<AuthAccountId> for EventId<AuthAccountEvent, AuthAccount> {
     fn from(auth_account_id: AuthAccountId) -> Self {
         EventId::new(auth_account_id.0)
--- kernel/src/entity/auth_host/id.rs
+++ kernel/src/entity/auth_host/id.rs
@@ -4,3 +4,9 @@ use vodca::{AsRefln, Fromln, Newln};
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Fromln, AsRefln, Newln, Serialize, Deserialize)]
 pub struct AuthHostId(Uuid);
+
+impl Default for AuthHostId {
+    fn default() -> Self {
+        AuthHostId(Uuid::now_v7())
+    }
+}
