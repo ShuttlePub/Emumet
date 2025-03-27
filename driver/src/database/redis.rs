72418daccaa2cc7221a76f475e88995e3a02e784
turtton <top.gear7509@turtton.net>
2025-03-27 22:05:24 +0900

    :hammer: Implement DatabaseConnection for RedisConnection

--- driver/src/database/redis.rs
+++ driver/src/database/redis.rs
@@ -1,7 +1,10 @@
 use crate::database::env;
 use deadpool_redis::{Config, Pool, Runtime};
 use error_stack::{Report, ResultExt};
+use kernel::interfaces::database::{DatabaseConnection, Transaction};
 use kernel::KernelError;
+use std::ops::Deref;
+use vodca::References;
 
 // redis://127.0.0.1
 const REDIS_URL: &str = "REDIS_URL";
@@ -9,7 +12,7 @@ const REDIS_URL: &str = "REDIS_URL";
 // 127.0.0.1
 const HOST: &str = "REDIS_HOST";
 
-#[derive(Clone)]
+#[derive(Clone, References)]
 pub struct RedisDatabase {
     pool: Pool,
 }
@@ -30,5 +33,30 @@ impl RedisDatabase {
             .create_pool(Some(Runtime::Tokio1))
             .change_context_lazy(|| KernelError::Internal)?;
         Ok(Self { pool })
+    }
+}
+
+pub struct RedisConnection(deadpool_redis::Connection);
+
+impl Transaction for RedisConnection {}
+
+impl Deref for RedisConnection {
+    type Target = deadpool_redis::Connection;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DatabaseConnection for RedisDatabase {
+    type Transaction = RedisConnection;
+
+    async fn begin_transaction(&self) -> error_stack::Result<Self::Transaction, KernelError> {
+        let pool = self
+            .pool
+            .get()
+            .await
+            .change_context_lazy(|| KernelError::Internal)?;
+        Ok(RedisConnection(pool))
     }
 }
