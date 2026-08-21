bb85c107f520673c5ef1a7c05d53715f89df9bb9
turtton <top.gear7509@turtton.net>
2026-08-22 01:34:57 +0900

    :sparkles: Add unban and reactivate account use cases (#49)

--- application/src/service/account/mod.rs
+++ application/src/service/account/mod.rs
@@ -2,12 +2,16 @@ mod create;
 mod deactivate;
 mod instance_role;
 mod moderation;
+mod reactivate;
 mod read;
 mod update;
 
 pub use create::CreateAccountUseCase;
 pub use deactivate::DeactivateAccountUseCase;
 pub use instance_role::{AssignInstanceRoleUseCase, RevokeInstanceRoleUseCase};
-pub use moderation::{BanAccountUseCase, SuspendAccountUseCase, UnsuspendAccountUseCase};
+pub use moderation::{
+    BanAccountUseCase, SuspendAccountUseCase, UnbanAccountUseCase, UnsuspendAccountUseCase,
+};
+pub use reactivate::ReactivateAccountUseCase;
 pub use read::GetAccountUseCase;
 pub use update::UpdateAccountUseCase;
